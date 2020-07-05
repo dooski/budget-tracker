e4adf01c8630e2de93c1eecb0c18f92c1b809b66
Dooski <dmckeon495@gmail.com>
2020-07-05 18:48:33 -0400

    fixed missing axios script

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,7 +17,6 @@ const DATA_CACHE_NAME = "data-cache"
 self.addEventListener('install', function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            console.log("Your files were pre-cached successfully!");
             return cache.addAll(FILES_TO_CACHE);
         })
     );
